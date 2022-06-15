1fb22ad6371c5753304b20f8aee8e6aeb821488f
CoffeePower100 <64426956+CoffeePower100@users.noreply.github.com>
2022-06-15 22:55:13 +0300

    added routes
    Added routes to account handling and category handling.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const database = require("./utilis/database");
+
+const app = express();
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+
+const accountsRoute = require('./controllers/account');
+app.use('/', accountsRoute);
+
+const productsRoute = require('./controllers/category');
+app.use('/category', productsRoute);
+
+const port = 5070;
+
+database
+.sync()
+.then(results => {
+    console.log(results);
+    app.listen(port, function(){
+        console.log(`Server working with port ${port}`);
+    })
+})
+.catch(err => console.log(err))
