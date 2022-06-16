92d42ba5ab3eee1c893065cf7599528a1ec742f4
CoffeePower100 <64426956+CoffeePower100@users.noreply.github.com>
2022-06-16 14:51:06 -0400

    current auth.js (works with no token and valid token)

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,29 +6,42 @@ const Account = require('../models/account');
 const auth = async(request, response, next) => {
 
     const bearerHeader = request.headers['authorization'];
-    const bearer = bearerHeader.split(" ");
-    const token = bearer[1];
+    
+    // if given token isn't string's type:
+    if ("string" == typeof(bearerHeader))
+    {
+        const bearer = bearerHeader.split(" ");
+    
+        const token = bearer[1];
 
-    const tokenVerify = await jsonWebToken.verify(token, 'KswkWJ3j4ljL2');
-    Account.findByPk(tokenVerify.email)
-    .then(account => {
-        if (null == account)
-        {
-            // Request from Unauthorized source:
-            return response.status(403).json({
-                message: "unautorized user"
+        const tokenVerify = jsonWebToken.verify(token, 'KswkWJ3j4ljL2');
+        Account.findByPk(tokenVerify.email)
+        .then(account => {
+            if (null == account)
+            {
+                // Request from Unauthorized source:
+                return response.status(403).json({
+                    message: "unautorized user"
+                });
+            }
+            else
+            {
+                next();
+            }
+        })
+        .catch(err => {
+            return response.status(500).json({
+                error: err
             });
-        }
-        else
-        {
-            next();
-        }
-    })
-    .catch(err => {
-        return response.status(500).json({
-            error: err
+        })
+    }
+    else
+    {
+        return response.status(200).json({
+            error: "given auth token isn't a string"
         });
-    })
+    }
+
 }
 
 
